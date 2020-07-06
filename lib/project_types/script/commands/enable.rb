f8a72af0232d8423eb7f0426c559399e42d35f6d
Spencer Haan <spencer.haan+github@shopify.com>
2020-07-06 17:36:59 -0400

    Added --config_file and --config_props flags for scripts enable

--- lib/project_types/script/commands/enable.rb
+++ lib/project_types/script/commands/enable.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module Script
   module Commands
     class Enable < ShopifyCli::Command
       options do |parser, flags|
         parser.on('--api_key=APIKEY') { |t| flags[:api_key] = t }
         parser.on('--shop_domain=MYSHOPIFYDOMAIN') { |t| flags[:shop_domain] = t }
+        parser.on('--config_props=KEYVALUEPAIRS', Array) do |t|
+          flags[:config_props] = Hash[t.map { |s| s.split(':') }]
+        end
+        parser.on('--config_file=CONFIGFILEPATH') { |t| flags[:config_file] = t }
       end
 
       def call(args, _name)
@@ -18,7 +24,7 @@ module Script
           ctx: @ctx,
           api_key: form.api_key,
           shop_domain: form.shop_domain,
-          configuration: { entries: [] },
+          configuration: acquire_configuration(**options.flags.slice(:config_file, :config_props)),
           extension_point_type: project.extension_point_type,
           title: project.script_name
         )
@@ -40,6 +46,23 @@ module Script
 
       def self.extended_help
         ShopifyCli::Context.message('script.enable.extended_help', ShopifyCli::TOOL_NAME)
+      end
+
+      private
+
+      def acquire_configuration(config_file: nil, config_props: nil)
+        properties = {}
+        properties = JSON.parse(File.read(config_file)) unless config_file.nil?
+        properties = properties.merge(config_props) unless config_props.nil?
+
+        configuration = { entries: [] }
+        properties.each do |key, value|
+          configuration[:entries].push({
+            key: key,
+            value: value,
+          })
+        end
+        configuration
       end
     end
   end
